65088533ca5541f9f87846650f66292ab838dfd3
damkols <damolausmankolapo@gmail.com>
2023-11-14 13:37:39 +0100

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,32 +5,31 @@ import "./index.css";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 const activeChain = {
- chainId: 11155111,
- rpc: ["https://sepolia.infura.io/v3/5b887901bcee46279c3803899a48c5a0"],
+  chainId: 11155111,
+  rpc: ["https://sepolia.infura.io/v3/5b887901bcee46279c3803899a48c5a0"],
 
- nativeCurrency: {
-  decimals: 18,
-  name: "Ether",
-  symbol: "ETH",
- },
- shortName: "Sepolia testnet",
- slug: "sepoliatest",
- testnet: true,
- chain: "Sepolia",
- name: "Sepolia Testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  shortName: "Sepolia testnet",
+  slug: "sepoliatest",
+  testnet: true,
+  chain: "Sepolia",
+  name: "Sepolia Testnet",
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
- <React.StrictMode>
-  <ThirdwebProvider
-   activeChain={activeChain}
-   //  clientId={"f0d9bfeee428cdd3ba34e13869e2fd12"}
-   clientId={import.meta.env.VITE_CLIENT_ID}
-   chainId={11155111}
-   supportedChains={[activeChain]}
-   autoConnect={false}
-  >
-   <App />
-  </ThirdwebProvider>
- </React.StrictMode>
+  <React.StrictMode>
+    <ThirdwebProvider
+      activeChain={activeChain}
+      clientId={import.meta.env.VITE_CLIENT_ID}
+      chainId={11155111}
+      supportedChains={[activeChain]}
+      autoConnect={false}
+    >
+      <App />
+    </ThirdwebProvider>
+  </React.StrictMode>
 );
